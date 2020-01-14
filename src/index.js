81f1d9de02e633c45766d2013ae48da13bb6de40
Anja Schaffhirt <a.schaffhirt@sedna-soft.de>
2020-01-14 12:56:32 +0000

    Add store to index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {Provider} from "react-redux";
+import {createStore} from 'redux';
+import calcGauApp from "./reducers";
 
+const store = createStore(
+    calcGauApp,
+    {
+        types: [
+            {name: "foo", levels: [{max: 1000, price: 3.45}, {max: null, price: 2.34}]},
+            {name: "bar", levels: [{max: null, price: 1.23}]},
+            {name: "baz", levels: [{max: 1000, price: 3.45}, {max: 20000, price: 2.34}, {max: null, price: 1.23}]}
+        ]
+    }
+);
 ReactDOM.render(
-    <Provider><App/></Provider>,
+    <Provider store={store}>
+        <App/>
+    </Provider>,
     document.getElementById('root')
 );
