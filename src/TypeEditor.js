dfc7c7670d1a1dd946fa0b50fc81b9bc2c98dd7c
Anja Schaffhirt <a.schaffhirt@sedna-soft.de>
2020-01-14 14:14:46 +0000

    Use level editor component within type editor

--- src/TypeEditor.js
+++ src/TypeEditor.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import {connect} from "react-redux";
 import {renameType} from "./actions";
+import LevelEditor from "./LevelEditor";
 
 function TypeEditor({name, levels, typeIndex, renameType}) {
     const onChange = e => renameType(typeIndex, e.target.value);
     return (
         <tr>
             <td className="type"><input autoFocus={true} type="text" value={name} onChange={onChange}/></td>
+            {levels.map((level, index) => <LevelEditor key={index} typeIndex={typeIndex} levelIndex={index}/>)}
             <td className="delete"><button tabIndex={-1}>×</button></td>
         </tr>
     );
