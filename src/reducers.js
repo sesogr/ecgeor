4a0015e10ae15c45a2bba8a80264bc63a8149bec
Anja Schaffhirt <a.schaffhirt@sedna-soft.de>
2020-01-14 14:12:51 +0000

    Update reducer to add the default level to every new type

--- src/reducers.js
+++ src/reducers.js
@@ -1,6 +1,6 @@
 function types (state = [], {type, payload}) {
     switch (type) {
-        case 'ADD_TYPE': return state.concat([{name: payload.name, levels: []}]);
+        case 'ADD_TYPE': return state.concat([{name: payload.name, levels: [{max: null, price: 0}]}]);
         case 'RENAME_TYPE': return state.map((type, index) =>
             index === payload.index
                 ? Object.assign({}, type, {name: payload.newName})
