2a993287625e7f9a65616e309b847159ea530e1f
Anja Schaffhirt <a.schaffhirt@sedna-soft.de>
2020-01-14 13:11:58 +0000

    Wire up app component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
 import './App.css';
+import {connect} from "react-redux";
+import {addType} from "./actions";
+import TypeEditor from "./TypeEditor";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App({types, addType}) {
+    const createTypeEditor = e => {
+        addType(e.target.value);
+        e.target.value = '';
+    };
+    return (
+        <table id="d486574ef229">
+            <tbody>
+            {types.map((type, index) => <TypeEditor key={index} index={index}/>)}
+            <tr>
+                <td className="type new">
+                    <input placeholder="neuer Typ" type="text" onChange={createTypeEditor}/>
+                </td>
+            </tr>
+            </tbody>
+        </table>
+    );
 }
 
-export default App;
+export default connect(
+    state => ({types: state.types}),
+    {addType}
+)(App);
