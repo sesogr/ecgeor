85049d3d61927fa274ef11b0869d15582e9ab6ba
Anja Schaffhirt <a.schaffhirt@sedna-soft.de>
2020-01-15 07:57:58 +0000

    Bind values in level editor, but drop formatting; allow deletion

--- src/LevelEditor.js
+++ src/LevelEditor.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import './App.css';
 import {connect} from "react-redux";
+import {deleteLevel, repriceLevel, shiftLevel} from "./actionCreators";
 
-function LevelEditor({max, price, typeIndex, levelIndex}) {
+function LevelEditor({max, price, typeIndex, levelIndex, repriceLevel, shiftLevel, deleteLevel}) {
     const format = (n, p) => {
         const [, head, tail] = Math.trunc(n).toString().match(/^(\d+?)((?:\d{3})*)$/);
         const result = parseInt(head) ? head + tail.replace(/(...)/g, '.$1') + ',' + (n * parseFloat('10e+' + p)).toString().substr(-p - 1, p) : '';
@@ -11,13 +12,25 @@ function LevelEditor({max, price, typeIndex, levelIndex}) {
     };
     return (
         <td className="level">
-            <button tabIndex={-1}>×</button>
+            <button tabIndex={-1} onClick={() => deleteLevel(typeIndex, levelIndex)}>×</button>
             <div className="max">
-                <input type="text" placeholder={'unbegrenzt'} pattern={'^\\d{1,3}((\\.\\d{3})*|(\\d{3})*)(,\\d{1,3})?$'} value={format(max, 1)}/>
+                <input
+                    type="text"
+                    placeholder={'unbegrenzt'}
+                    pattern={'^\\d{1,3}((\\.\\d{3})*|(\\d{3})*)(,\\d{1,3})?$'}
+                    value={max}
+                    onChange={e => shiftLevel(typeIndex, levelIndex, e.target.value)}
+                />
                 <div className="unit">kWh</div>
             </div>
             <div className="price">
-                <input type="text" placeholder={'0'} pattern={'^\\d{1,3}((\\.\\d{3})*|(\\d{3})*)(,\\d{1,3})?$'} value={format(price, 3)}/>
+                <input
+                    type="text"
+                    placeholder={'0'}
+                    pattern={'^\\d{1,3}((\\.\\d{3})*|(\\d{3})*)(,\\d{1,3})?$'}
+                    value={price}
+                    onChange={e => repriceLevel(typeIndex, levelIndex, e.target.value)}
+                />
                 <div className="unit">¢/kWh</div>
             </div>
         </td>
@@ -26,5 +39,5 @@ function LevelEditor({max, price, typeIndex, levelIndex}) {
 
 export default connect(
     (state, props) => state.types[props.typeIndex].levels[props.levelIndex],
-    {}
+    {repriceLevel, shiftLevel, deleteLevel}
 )(LevelEditor);
