ee9465a6f2848bd172cc33b66f59cd619d1c4da0
Anja Schaffhirt <a.schaffhirt@sedna-soft.de>
2020-01-15 07:55:22 +0000

    Add more actions for both type and level

--- src/actionCreators.js
+++ src/actionCreators.js
@@ -1,4 +1,4 @@
-import {ADD_TYPE, RENAME_TYPE} from "./actionTypes";
+import {ADD_LEVEL, ADD_TYPE, DELETE_LEVEL, DELETE_TYPE, RENAME_TYPE, REPRICE_LEVEL, SHIFT_LEVEL} from "./actionTypes";
 
 export function addType(name) {
     return {
@@ -6,9 +6,45 @@ export function addType(name) {
         payload: {name}
     };
 }
+
 export function renameType(typeIndex, newName) {
     return {
         type: RENAME_TYPE,
         payload: {typeIndex, newName}
+    };
+}
+
+export function deleteType(typeIndex) {
+    return {
+        type: DELETE_TYPE,
+        payload: {typeIndex}
+    };
+}
+
+export function addLevel(typeIndex, previousLevelIndex, min) {
+    return {
+        type: ADD_LEVEL,
+        payload: {typeIndex, previousLevelIndex, min}
+    };
+}
+
+export function shiftLevel(typeIndex, levelIndex, newMax) {
+    return {
+        type: SHIFT_LEVEL,
+        payload: {typeIndex, levelIndex, newMax}
+    };
+}
+
+export function repriceLevel(typeIndex, levelIndex, newPrice) {
+    return {
+        type: REPRICE_LEVEL,
+        payload: {typeIndex, levelIndex, newPrice}
+    };
+}
+
+export function deleteLevel(typeIndex, levelIndex) {
+    return {
+        type: DELETE_LEVEL,
+        payload: {typeIndex, levelIndex}
     };
 }
