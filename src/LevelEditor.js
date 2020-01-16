5da72636e293860dfd64b82ad70cf1dfcd53839c
Anja Schaffhirt <a.schaffhirt@sedna-soft.de>
2020-01-16 23:35:45 +0000

    Implement number formatting and validation as uncontrolled component

--- src/LevelEditor.js
+++ src/LevelEditor.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {connect} from "react-redux";
 import {deleteLevel, repriceLevel, shiftLevel} from "./actionCreators";
 
 function LevelEditor({max, price, typeIndex, levelIndex, repriceLevel, shiftLevel, deleteLevel}) {
+    const formatNumber = (n, p) => {
+        const value = ('' + n).replace(/^0+(.)/, '$1').replace(/[^\d,]+/g, '').replace(/(,[^,]*),.*/g, '$1').replace(/^,/, '0,');
+        return value.match(/^0?$/)
+            ? value
+            : value[0]
+            + value
+                .replace(/^.|,.*/g, '').split('').reverse().join('')
+                .replace(/(\d{3})/g, '$1.').split('').reverse().join('')
+            + value.replace(/^[^,]+/, '').substr(0, p + 1);
+    };
+    const [maxState, setMaxState] = useState(formatNumber(max, 1));
+    const handleMaxChange = e => setMaxState(formatNumber(e.target.value, 1));
     return (
         <td className="level">
             <button tabIndex={-1} onClick={() => deleteLevel(typeIndex, levelIndex)}>×</button>
@@ -12,8 +24,9 @@ function LevelEditor({max, price, typeIndex, levelIndex, repriceLevel, shiftLeve
                     type="text"
                     placeholder={'unbegrenzt'}
                     pattern={'^\\d{1,3}((\\.\\d{3})*|(\\d{3})*)(,\\d{1,3})?$'}
-                    value={max}
-                    onChange={e => shiftLevel(typeIndex, levelIndex, e.target.value)}
+                    value={maxState}
+                    onBlur={() => shiftLevel(typeIndex, levelIndex, maxState ? maxState.replace(/\./g, '').replace(/,/, '.') - 0 : null)}
+                    onChange={handleMaxChange}
                 />
                 <div className="unit">kWh</div>
             </div>
