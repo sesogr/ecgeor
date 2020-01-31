26dac2533af0ce32c7622080483e6956345ad1b5
Anja <a.schaffhirt@sedna-soft.de>
2020-01-31 06:12:56 +0000

    Reformat TypeEditor

--- src/TypeEditor.js
+++ src/TypeEditor.js
@@ -4,18 +4,50 @@ import {connect} from "react-redux";
 import {changeDefaultStateOfType, deleteType, renameType} from "./actionCreators";
 import LevelEditor from "./LevelEditor";
 
-function TypeEditor({name, defaultActive, levels, revision = 0, typeIndex, renameType, deleteType, changeDefaultStateOfType, exclude}) {
+function TypeEditor(
+    {
+        name,
+        defaultActive,
+        levels,
+        revision = 0,
+        typeIndex,
+        renameType,
+        deleteType,
+        changeDefaultStateOfType,
+        exclude
+    }
+) {
     return (
         <tr>
-            <td className="type"><input autoFocus={true} type="text" value={name} onChange={e => renameType(typeIndex, e.target.value)}/></td>
+            <td className="type">
+                <input
+                    autoFocus={true}
+                    type="text"
+                    value={name}
+                    onChange={e => renameType(typeIndex, e.target.value)}
+                />
+            </td>
             <td className="flag">
                 <label title="Standardmäßig aktiv?">
                     aktiv?<br/>
-                    <input type="checkbox" defaultChecked={defaultActive} onChange={e => changeDefaultStateOfType(typeIndex, e.target.checked)}/>
+                    <input
+                        type="checkbox"
+                        defaultChecked={defaultActive}
+                        onChange={e => changeDefaultStateOfType(typeIndex, e.target.checked)}
+                    />
                 </label>
             </td>
-            {levels.map((level, index) => <LevelEditor key={revision + ':' + index} exclude={levels.map(l => l.max)} typeIndex={typeIndex} levelIndex={index}/>)}
-            <td className="delete"><button onClick={() => deleteType(typeIndex)} tabIndex={-1}>×</button></td>
+            {levels.map((level, index) => (
+                <LevelEditor
+                    key={revision + ':' + index}
+                    exclude={levels.map(l => l.max)}
+                    typeIndex={typeIndex}
+                    levelIndex={index}
+                />
+            ))}
+            <td className="delete">
+                <button onClick={() => deleteType(typeIndex)} tabIndex={-1}>×</button>
+            </td>
         </tr>
     );
 }
